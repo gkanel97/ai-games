d3b457006593abbaebc8b38ad5c70b8cc90ba861
George Kanellopoulos <kanellog@tcd.ie>
2024-04-02 01:56:22 +0100

    Implemented random opponent

---
+++ TicTacToeRandomSolver.py
@@ -0,0 +1,15 @@
+import numpy as np
+from TicTacToeSolver import TicTacToeSolver
+
+class TicTacToeRandomSolver(TicTacToeSolver):
+
+    def __init__(self, game_instance):
+        self.game = game_instance
+    
+    def computer_turn(self):
+        action = np.random.choice(9)
+        logical_position = (action // 3, action % 3)
+        while self.game.is_grid_occupied(logical_position):
+            action = np.random.choice(9)
+            logical_position = (action // 3, action % 3)    
+        self.append_computer_move(logical_position)
